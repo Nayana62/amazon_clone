ae4b35309e882d4fb25ac69e4ca58825f02531a8
Nayana62 <nmnayana62@gmail.com>
2023-06-15 11:37:01 +0530

    modified: firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyAgPyMtkh55jOqRg5oQbFuqBQ-uhhdtxvQ",
+  authDomain: "clone-ffc5a.firebaseapp.com",
+  projectId: "clone-ffc5a",
+  storageBucket: "clone-ffc5a.appspot.com",
+  messagingSenderId: "255023384944",
+  appId: "1:255023384944:web:35e05507a33901af2867b5"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
